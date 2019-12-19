d58639d3d91a61ae8662ca054f084a51060ec91b
WhiteMinds <WhiteMind@qq.com>
2019-12-19 16:05:03 +0800

    Demo2

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react'
-import { View, Text, Image } from 'remax/wechat'
+import { View, Text } from 'remax/wechat'
 import styles from './index.module.css'
 
 export default () => {
   return (
-    <View className={styles.app}>
-      <View className={styles.header}>
-        <Image
-          src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-          className={styles.logo}
-        />
-        <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.tsx</Text> 开始
-        </View>
-      </View>
+    <View className={styles.app}
+          style={{
+            '---color1': 'red',
+            '--color2': 'blue'
+          }}>
+      <Text className={styles.color1}>Color1</Text>
+      <Text className={styles.color2}>Color2</Text>
     </View>
-  );
-};
+  )
+}
